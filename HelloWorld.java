b5b0e0df52dedfdbcb9eb719e3cd5c78e830e5b0
MinhHoang10 <133867691+MinhHoang10@users.noreply.github.com>
2023-11-18 11:32:36 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// Nguyen Huy Hoang - 20215056
-public class HelloWorld {
-    public static void main(String args[]){
-        System.out.println("Nguyen Huy Hoang - 5056");
-        System.out.println("Xin chao \n cac ban");
-        System.out.println("Hello \t World");
-    }
-}
