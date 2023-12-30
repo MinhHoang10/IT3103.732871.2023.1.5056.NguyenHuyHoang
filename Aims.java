14c0b7f32df66ada9178e9482aa2057d47587db8
MinhHoang10 <133867691+MinhHoang10@users.noreply.github.com>
2023-12-30 22:22:20 +0700

    Add files via upload

--- Aims.java
+++ Aims.java
@@ -1,30 +1,76 @@
-package AimsProject;
+package hust.soict.dsai.aims;
+import java.util.ArrayList;
+import java.util.List;
+
+
+// Nguyen Huy Hoang - 20215056
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Track;
+import hust.soict.dsai.aims.screen.StoreScreen;
+import hust.soict.dsai.aims.store.Store;
+
 public class Aims {
-    public static void main(String[] args){
-        cart anOrder = new cart();//create a new cart
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation",
-                                  "Roger Allers",87,19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
+	
+	public static void main(String[] args) {
+		
+		// Khởi tạo một vài Media cho Store
+		List<String> tacgia = new ArrayList<String>();
+		tacgia.add("Hoan");
+		tacgia.add("Nam");
+	
+		List<String> tacgia2 = new ArrayList<String>();
+		tacgia.add("Hoan");
+	
+		
+		List<Track> tr = new ArrayList<Track>();
+		
+		// Khởi tạo dvd
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Luffy", "Animation", "Oda", 120, 100f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Spiderman", "ACtion", "Marvel", 144, 37.15f);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
-                                "George Lucas", 87, 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
+		// Khởi tạo book
+		Book book1 = new Book("Hahaha", "Hai", 20f, tacgia );
+		Book book2 = new Book("Vaaa", "Hai", 20f, tacgia );
+		Book book3 = new Book("How to make a book", "Xamxi", 12f, tacgia2 );
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
-        anOrder.addDigitalVideoDisc(dvd3);
-
-        System.out.println();
-        anOrder.displayCart(); // Cart Display DVD details
-
+		// Khởi tạo cd
+		CompactDisc cd1 = new CompactDisc("Nghe di", "Hoan", tr, 15f );
+		CompactDisc cd2 = new CompactDisc("Nghe di2", "Hai", "Hoan", 16f );
+		CompactDisc cd3 = new CompactDisc("Rappp", "Trap", "Hoan", 33f );
 
 
-        //print total cost of the items in the cart
-        System.out.println("Total Cost is : ");
-        System.out.println(anOrder.totalCost());
+		// thêm vài track vào cd
+		Track tr1 = new Track("track1", 12);
+		Track tr2 = new Track("track2", 3);
+		Track tr3 = new Track("track3", 5);
+		Track tr4 = new Track("track4", 2);
+		cd1.addTrack(tr1);
+		cd2.addTrack(tr2);
+		cd1.addTrack(tr3);
+		cd2.addTrack(tr4);
+				
+		// tạo store và cart
+		Store store = new Store(); // tạo store
+		Cart cart = new Cart(); // tạo cart
+		
+		// thêm vào store
+		store.addMedia(dvd1);
+		store.addMedia(dvd2);
+		store.addMedia(cd2);
+		store.addMedia(book1);
+		store.addMedia(book2);
+		store.addMedia(cd1);
+		store.addMedia(dvd3);
+		store.addMedia(cd3);
+		store.addMedia(book3);
 
-        //Remove DVD from cart
-        System.out.println();
-        anOrder.removeDigitalVideoDisc(dvd2);
-        anOrder.displayCart();
-    }
+		new StoreScreen(store, cart);
+
+	}
+	
 }
+	
